39b6e3f8aacac398377229ab790fe9b84720b360
dahye4368 <dahye4368@daum.net>
2023-03-16 17:59:47 +0900

    초기 커밋.

---
+++ YangDaHye_ex03/Three_d.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YangDaHye_ex03
+{
+    class Three_d
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //while (a > 0)
+            //{
+            //    a--;
+
+            //    int b = 0;
+            //    while (b <= a)
+            //    {
+            //        b--;
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
+        }
+    }
+}
---
+++ YangDaHye_ex03_while/Tree_w.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YangDaHye_ex03_while
+{
+    class Tree_w
+    {
+        static void Main(string[] args)
+        {
+            //int a = 5;
+            //while (a > 0)
+            //{
+            //    a--;
+
+            //    int b = 0;
+            //    while (b >= a)
+            //    {
+            //        b++;
+            //        {
+            //            Console.Write("*");
+            //        }
+            //        Console.WriteLine();
+            //    }
+            //}
+        }
+    }
+}
