ae3e3a446068943f88bb65a56a47c09f6933b9a4
dahye4368 <dahye4368@daum.net>
2023-03-17 15:47:04 +0900

    ㅎㅎ

---
+++ Continue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <10; i ++)
+            {
+                if (i % 2 == 0)
+                    continue;
+
+                Console.WriteLine($"{i}:홀수");
+            }
+        }
+    }
+}
+//짝수는 if문 조건과 cintinue에 의해 출력되지 않고 1,2,5,7,9만 줄줄이 1:홀수 식으로 출력
